7760d2ff216dfeaba6badd6ea0862886a339b710
dhouck <Software@DRHouck.me>
2026-08-19 23:41:02 +0000

    Build: (11315f8) Fix GH Actions

---
+++ scripts/github-workflow-permissions-test/github-workflow-permissions-test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var kolmafia = require('kolmafia');
+
+function main() {
+  kolmafia.print("You have successfully built github-workflow-permissions-test!");
+}
+
+exports.main = main;
